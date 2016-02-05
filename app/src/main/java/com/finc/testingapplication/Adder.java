a78216f938e8a9e1afed541efb3a8108a755f6cf
P Chan <shiroyamaryu@gmail.com>
2016-02-05 15:03:55 +0900

    initialization

--- app/src/main/java/com/finc/testingapplication/Adder.java
+++ app/src/main/java/com/finc/testingapplication/Adder.java
@@ -4,7 +4,7 @@ package com.finc.testingapplication;
  * FiNC Testing App
  */
 public class Adder {
-    public String func(String x, String y) {
+    public static String func(String x, String y) {
         int x1 = Integer.parseInt(x);
         int y1 = Integer.parseInt(y);
 
